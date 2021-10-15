49ac7f3985e6f83e8d98ffb44bc006d3eee07865
Chikobvore <nchikobvor@gmail.com>
2021-10-15 12:46:00 +0200

    changed api

--- api.py
+++ api.py
@@ -5,20 +5,22 @@ api_token = 'z5juamogjbjj9yih'
 
 #EMAIL: chigumbubyron@gmail.com
 def reply_message(sender,message):
-      payload = {
-            "phone": sender,
-            "body": message
-            }
+      return '',200
+      # payload = {
+      #       "phone": sender,
+      #       "body": message
+      #       }
 
-      response = requests.post( api_instance_url+"/sendMessage?token="+api_token, data=payload)
-      return str(response.status_code)
+      # response = requests.post( api_instance_url+"/sendMessage?token="+api_token, data=payload)
+      # return str(response.status_code)
 
 def send_attachment(sender,attachment_url,caption):
-      payload = {
-            "phone": sender,
-            "filename": caption,
-            "caption": caption,
-            "body": attachment_url
-      }
-      response = requests.post(api_instance_url+"/sendFile?token="+api_token, data=payload)
-      return str(response.status_code)
+      return '',200
+      # payload = {
+      #       "phone": sender,
+      #       "filename": caption,
+      #       "caption": caption,
+      #       "body": attachment_url
+      # }
+      # response = requests.post(api_instance_url+"/sendFile?token="+api_token, data=payload)
+      # return str(response.status_code)
